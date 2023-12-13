b507a03ee6ffaf0fadb28c20fa45d02b09894ab7
Didier Toussaint <didier.toussaint.trad@gmail.com>
2023-12-13 06:23:15 +0100

    Day 12 - Part 1

---
+++ day-12/part-1/index.js
@@ -0,0 +1,55 @@
+import Line   from './Line.js'
+import values from './../values.js'
+
+const day  = 12
+const part = 1
+
+const {testInput, input} = values
+const expectedTestResult = 21
+
+class Task {
+  constructor(input) {
+    this.input = input
+    this.lines = []
+  }
+
+  run() {
+    this.prepareLines()
+
+    return this.lines.reduce((acc, line) => {
+      return acc + this.processLine(line)
+    }, 0)
+  }
+
+  processLine(line) {
+    // Bit of cleaning first
+    line.clearAllTrailingGroups()
+    line.clearAllLeadingGroups()
+
+    return line.countTotalArrangements()
+  }
+
+  prepareLines() {
+    let lines = this.input.replace(/^\n/,     '')
+                          .replace(/\n$/,     '')
+                          .split('\n')
+
+    this.lines = lines.map((line) => new Line(line))
+  }
+}
+
+console.log(`# Day ${day} - Part ${part} #`)
+console.log('----------------')
+
+const testResult = new Task(testInput).run()
+
+console.log(' Test:')
+console.log(' - Expected result: ' + expectedTestResult)
+console.log(' - Computed result: ' + testResult)
+
+console.log('----------------')
+
+const realResult = new Task(input).run()
+
+console.log(' Real task:')
+console.log(' - Computed result: ' + realResult)

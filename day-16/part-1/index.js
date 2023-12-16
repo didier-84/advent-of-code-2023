e649a329c6ba67c9e1866d966cbf197dce547268
Didier Toussaint <didier.toussaint.trad@gmail.com>
2023-12-16 21:44:10 +0100

    Refactor and optimize Day 16

--- day-16/part-1/index.js
+++ day-16/part-1/index.js
@@ -11,7 +11,7 @@ class Task {
     this.input = input
     this.tiles = {}
 
-    this.history   = []
+    this.history   = {}
     this.nextTiles = []
 
     this.energized = new Set()
@@ -41,21 +41,11 @@ class Task {
     let tile = this.tiles[tileKey]
 
     switch (tile.char) {
-      case '.':
-        this.letBeamThrough(tile, direction)
-        break
-      case '|':
-        this.processVerticalPipeTile(tile, direction)
-        break
-      case '-':
-        this.processHorizontalPipeTile(tile, direction)
-        break
-      case '╲':
-        this.processBackslashTile(tile, direction)
-        break
-      case '╱':
-        this.processSlashTile(tile, direction)
-        break
+      case '.': return this.letBeamThrough(tile, direction)
+      case '|': return this.processVerticalPipeTile(tile, direction)
+      case '-': return this.processHorizontalPipeTile(tile, direction)
+      case '╲': return this.processBackslashTile(tile, direction)
+      case '╱': return this.processSlashTile(tile, direction)
     }
   }
 
@@ -63,7 +53,7 @@ class Task {
     if ('NS'.includes(direction)) {
       this.letBeamThrough(tile, direction)
     } else {
-      this.splitBeam(tile)
+      this.splitBeamVertically(tile)
     }
   }
 
@@ -71,112 +61,61 @@ class Task {
     if ('WE'.includes(direction)) {
       this.letBeamThrough(tile, direction)
     } else {
-      this.splitBeam(tile)
+      this.splitBeamHorizontally(tile)
     }
   }
 
   processBackslashTile(tile, direction) {
-    let nextTileKey, newDirection = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey  = `${tile.row}-${tile.col - 1}`
-        newDirection = 'W'
-        break
-      case 'S':
-        nextTileKey  = `${tile.row}-${tile.col + 1}`
-        newDirection = 'E'
-        break
-      case 'E':
-        nextTileKey  = `${tile.row + 1}-${tile.col}`
-        newDirection = 'S'
-        break
-      case 'W':
-        nextTileKey  = `${tile.row - 1}-${tile.col}`
-        newDirection = 'N'
-        break
+      case 'N': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+      case 'S': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
+      case 'E': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
+      case 'W': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
     }
-
-    return this.addToNextTiles(nextTileKey, newDirection)
   }
 
   processSlashTile(tile, direction) {
-    let nextTileKey, newDirection = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey  = `${tile.row}-${tile.col + 1}`
-        newDirection = 'E'
-        break
-      case 'S':
-        nextTileKey  = `${tile.row}-${tile.col - 1}`
-        newDirection = 'W'
-        break
-      case 'E': // /
-        nextTileKey  = `${tile.row - 1}-${tile.col}`
-        newDirection = 'N'
-        break
-      case 'W':
-        nextTileKey  = `${tile.row + 1}-${tile.col}`
-        newDirection = 'S'
-        break
+      case 'N': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
+      case 'S': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+      case 'E': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
+      case 'W': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
     }
-
-    return this.addToNextTiles(nextTileKey, newDirection)
   }
 
   letBeamThrough(tile, direction) {
-    let nextTileKey = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey = `${tile.row - 1}-${tile.col}`
-        break
-      case 'S':
-        nextTileKey = `${tile.row + 1}-${tile.col}`
-        break
-      case 'E':
-        nextTileKey = `${tile.row}-${tile.col + 1}`
-        break
-      case 'W':
-        nextTileKey = `${tile.row}-${tile.col - 1}`
-        break
+      case 'N': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, direction)
+      case 'S': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, direction)
+      case 'E': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, direction)
+      case 'W': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, direction)
     }
-    return this.addToNextTiles(nextTileKey, direction)
   }
 
-  splitBeam(tile) {
-    let keyA, keyB = ''
+  splitBeamVertically(tile) {
+    this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
+    this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
+  }
 
-    if (tile.char == '-') {
-      keyA = `${tile.row}-${tile.col - 1}`
-      keyB = `${tile.row}-${tile.col + 1}`
-
-      this.addToNextTiles(keyA, 'W')
-      this.addToNextTiles(keyB, 'E')
-    } else {
-      keyA = `${tile.row - 1}-${tile.col}`
-      keyB = `${tile.row + 1}-${tile.col}`
-
-      this.addToNextTiles(keyA, 'N')
-      this.addToNextTiles(keyB, 'S')
-    }
+  splitBeamHorizontally(tile) {
+    this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+    this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
   }
 
   addToNextTiles(tileKey, direction) {
     let key = JSON.stringify(tileKey + direction)
-    if (this.history.indexOf(key) >= 0) return
+    if (this.history[key] !== undefined) return
 
-    this.history.push(key)
+    this.history[key] = 1
     this.nextTiles.push({ tileKey, direction })
   }
 
   prepareTiles() {
     let rows = this.input.replace(/^\n/, '')
-                           .replace(/\n$/, '')
-                           .replace(/\\/g, '╲')
-                           .replace(/\//g, '╱')
-                           .split('\n')
+                         .replace(/\n$/, '')
+                         .replace(/\\/g, '╲')
+                         .replace(/\//g, '╱')
+                         .split('\n')
 
     for (let i = 0; i < rows.length; i++) {
       for (let j = 0; j < rows[0].length; j++) {
--- day-16/part-2/index.js
+++ day-16/part-2/index.js
@@ -13,7 +13,7 @@ class Task {
 
     this.startsByDirection = { N: [], E: [], S:[], W:[] }
 
-    this.history   = []
+    this.history   = {}
     this.nextTiles = []
 
     this.energized = new Set()
@@ -28,7 +28,7 @@ class Task {
       let starts = this.startsByDirection[direction]
 
       starts.forEach((tileKey) => {
-        this.history = []
+        this.history = {}
         this.followBeam(tileKey, direction)
 
         while (this.nextTiles.length > 0) {
@@ -55,21 +55,11 @@ class Task {
     let tile = this.tiles[tileKey]
 
     switch (tile.char) {
-      case '.':
-        this.letBeamThrough(tile, direction)
-        break
-      case '|':
-        this.processVerticalPipeTile(tile, direction)
-        break
-      case '-':
-        this.processHorizontalPipeTile(tile, direction)
-        break
-      case '╲':
-        this.processBackslashTile(tile, direction)
-        break
-      case '╱':
-        this.processSlashTile(tile, direction)
-        break
+      case '.': return this.letBeamThrough(tile, direction)
+      case '|': return this.processVerticalPipeTile(tile, direction)
+      case '-': return this.processHorizontalPipeTile(tile, direction)
+      case '╲': return this.processBackslashTile(tile, direction)
+      case '╱': return this.processSlashTile(tile, direction)
     }
   }
 
@@ -77,7 +67,7 @@ class Task {
     if ('NS'.includes(direction)) {
       this.letBeamThrough(tile, direction)
     } else {
-      this.splitBeam(tile)
+      this.splitBeamVertically(tile)
     }
   }
 
@@ -85,112 +75,61 @@ class Task {
     if ('WE'.includes(direction)) {
       this.letBeamThrough(tile, direction)
     } else {
-      this.splitBeam(tile)
+      this.splitBeamHorizontally(tile)
     }
   }
 
   processBackslashTile(tile, direction) {
-    let nextTileKey, newDirection = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey  = `${tile.row}-${tile.col - 1}`
-        newDirection = 'W'
-        break
-      case 'S':
-        nextTileKey  = `${tile.row}-${tile.col + 1}`
-        newDirection = 'E'
-        break
-      case 'E':
-        nextTileKey  = `${tile.row + 1}-${tile.col}`
-        newDirection = 'S'
-        break
-      case 'W':
-        nextTileKey  = `${tile.row - 1}-${tile.col}`
-        newDirection = 'N'
-        break
+      case 'N': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+      case 'S': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
+      case 'E': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
+      case 'W': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
     }
-
-    return this.addToNextTiles(nextTileKey, newDirection)
   }
 
   processSlashTile(tile, direction) {
-    let nextTileKey, newDirection = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey  = `${tile.row}-${tile.col + 1}`
-        newDirection = 'E'
-        break
-      case 'S':
-        nextTileKey  = `${tile.row}-${tile.col - 1}`
-        newDirection = 'W'
-        break
-      case 'E': // /
-        nextTileKey  = `${tile.row - 1}-${tile.col}`
-        newDirection = 'N'
-        break
-      case 'W':
-        nextTileKey  = `${tile.row + 1}-${tile.col}`
-        newDirection = 'S'
-        break
+      case 'N': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
+      case 'S': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+      case 'E': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
+      case 'W': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
     }
-
-    return this.addToNextTiles(nextTileKey, newDirection)
   }
 
   letBeamThrough(tile, direction) {
-    let nextTileKey = ''
-
     switch(direction) {
-      case 'N':
-        nextTileKey = `${tile.row - 1}-${tile.col}`
-        break
-      case 'S':
-        nextTileKey = `${tile.row + 1}-${tile.col}`
-        break
-      case 'E':
-        nextTileKey = `${tile.row}-${tile.col + 1}`
-        break
-      case 'W':
-        nextTileKey = `${tile.row}-${tile.col - 1}`
-        break
+      case 'N': return this.addToNextTiles(`${tile.row - 1}-${tile.col}`, direction)
+      case 'S': return this.addToNextTiles(`${tile.row + 1}-${tile.col}`, direction)
+      case 'E': return this.addToNextTiles(`${tile.row}-${tile.col + 1}`, direction)
+      case 'W': return this.addToNextTiles(`${tile.row}-${tile.col - 1}`, direction)
     }
-    return this.addToNextTiles(nextTileKey, direction)
   }
 
-  splitBeam(tile) {
-    let keyA, keyB = ''
+  splitBeamVertically(tile) {
+    this.addToNextTiles(`${tile.row - 1}-${tile.col}`, 'N')
+    this.addToNextTiles(`${tile.row + 1}-${tile.col}`, 'S')
+  }
 
-    if (tile.char == '-') {
-      keyA = `${tile.row}-${tile.col - 1}`
-      keyB = `${tile.row}-${tile.col + 1}`
-
-      this.addToNextTiles(keyA, 'W')
-      this.addToNextTiles(keyB, 'E')
-    } else {
-      keyA = `${tile.row - 1}-${tile.col}`
-      keyB = `${tile.row + 1}-${tile.col}`
-
-      this.addToNextTiles(keyA, 'N')
-      this.addToNextTiles(keyB, 'S')
-    }
+  splitBeamHorizontally(tile) {
+    this.addToNextTiles(`${tile.row}-${tile.col - 1}`, 'W')
+    this.addToNextTiles(`${tile.row}-${tile.col + 1}`, 'E')
   }
 
   addToNextTiles(tileKey, direction) {
     let key = JSON.stringify(tileKey + direction)
-    if (this.history.indexOf(key) >= 0) return
+    if (this.history[key] !== undefined) return
 
-    this.history.push(key)
+    this.history[key] = 1
     this.nextTiles.push({ tileKey, direction })
   }
 
   prepareTiles() {
     let rows = this.input.replace(/^\n/, '')
-                           .replace(/\n$/, '')
-                           .replace(/\\/g, '╲')
-                           .replace(/\//g, '╱')
-                           .split('\n')
+                         .replace(/\n$/, '')
+                         .replace(/\\/g, '╲')
+                         .replace(/\//g, '╱')
+                         .split('\n')
 
     let lastRow = rows.length - 1
     let lastCol = rows[0].length - 1
